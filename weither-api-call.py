5219777f69f00c16157241ad1d4b0de3928b0cd5
AILogos <masterofskyart@gmail.com>
2023-04-30 22:09:06 +0200

    TP2 OK

--- weither-api-call.py
+++ weither-api-call.py
@@ -1,7 +1,14 @@
 import requests
 import os
+from fastapi import FastAPI
 
+
+app = FastAPI()
+
+
+@app.get("/")
 def get_weather(lat, lon, api_key):
+
     url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
     response = requests.get(url)
     if response.status_code == 200:
